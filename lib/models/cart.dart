c3fdb80c7f41837c08b9aa51c7c0b6ededcfa192
pranjulknit <pranjul.19641@knit.ac.in>
2021-12-16 13:58:51 +0530

    day23 get,set,fold,scaffold Messenger

---
+++ lib/models/cart.dart
@@ -0,0 +1,39 @@
+import '../models/catalog.dart';
+import 'catalog.dart';
+
+class CartModel {
+  //catalog field
+  late CatalogModel _catalog;
+
+  // Collection of Ids - storeIds of eachItem
+  final List<int> _itemIds=[];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get items in the cart
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  // Get Total Price for
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //Add Item
+  void add(Item item){
+    _itemIds.add(item.id);
+  }
+
+  //Remove Item
+
+  void remove (Item item){
+    _itemIds.remove(item.id);
+  }
+
+
+}
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,11 +3,11 @@ class CatalogModel {
   
 
   // getting Item by ID
-  static Item getById(int id) =>
+    Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
   // getting Item by Position
-   static Item getByPosition(int pos) => items[pos];
+   Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
